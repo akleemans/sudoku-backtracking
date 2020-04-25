2fad4b472badaab00b06ae29fd1efea83563ba56
Adrianus Kleemans <a.kleemans@gmail.com>
2020-04-25 09:51:52 +0200

    Rework setting peers of cell

--- sudoku.py
+++ sudoku.py
@@ -1,18 +1,21 @@
 from __future__ import annotations
 
 from tkinter import *
-from typing import Tuple
+from typing import List, Set
 
 from cell import Cell
 
 
 class Sudoku:
+    cells: List[Cell] = []
+    units: List[List[Cell]] = []
+    solutions: List[str] = []
 
     def __init__(self, sudoku_str: str):
         # Init cells
-        self.cells = []
         for i in range(len(sudoku_str)):
-            cell = Cell(sudoku_str[i], i)
+            value = sudoku_str[i]
+            cell = Cell(value, i)
             self.cells.append(cell)
 
         # Build peers list
@@ -20,59 +23,124 @@ class Sudoku:
             # row
             cell = self.cells[i]
             idx = i - i % 9
-            row = [cell for cell in self.cells[idx:idx + 9]]
+            row = set(c for c in self.cells[idx:idx + 9] if c != cell)
             cell.add_peers(row)
 
-            # col
+            # column
             cell = self.cells[i]
             base_idx = i % 9
-            col = []
+            col = set()
             for j in range(9):
                 idx = j * 9 + base_idx
-                col.append(self.cells[idx])
+                c = self.cells[idx]
+                if c != cell:
+                    col.add(c)
             cell.add_peers(col)
 
-            # TODO 3x3 block
-            block_y = (i % 9) - i % 3
-            block_x = (i - i % 9) % 3  # ???
+            # 3x3 block
+            block = set(self.cells[idx] for idx in self.get_block_ids(i) if idx != i)
+            print('For cell', i, ': adding block', ', '.join([str(cell.cell_id) for cell in block]))
+            cell.add_peers(block)
 
-            block = []
+    @staticmethod
+    def get_block_ids(idx: int) -> Set[int]:
+        """Return list of cell indexes for block"""
+        block_idx = set()
+        block_y = (idx % 9) - idx % 3
+        line_start = int((idx - idx % 9) / 9)
+        block_x = line_start - line_start % 3
+        for x in range(3):
             for y in range(3):
-                for x in range(3):
-                    idx = (block_x + x) * 9 + block_y + y
-                    block.append(self.cells[idx])
-            # cell.add_peers(block)
+                idx = (block_x + x) * 9 + block_y + y
+                block_idx.add(idx)
+        return block_idx
 
     def solve(self):
         """Solve Sudoku"""
         # initial propagation
         self.propagate()
 
-        # Add current state to
-        # stack = [self.serialize()]
-        # TODO try with backtracking
+        """
+        # Add current state to stack
+        stack: List[Tuple[List[str], Optional[Tuple[int, int]]]] = [(self.serialize(), None)]
+        # Work on stack with Depth-First-Search (DFS)
+        while len(stack) > 0:
+            sudoku, last_guess = stack[-1]
+            # TODO calculate list of possible guesses
+            possible_guesses = []
+            if possible_guesses[-1] == last_guess:
+                # End is reached in this branch
+                stack.pop()
+                continue
+            elif last_guess is None:
+                # Try first guess
+                next_guess = possible_guesses[0]
+            else:
+                idx = possible_guesses.index(last_guess)
+                next_guess = possible_guesses[idx + 1]
+            # TODO deserialize
+            s = Sudoku('')
+
+            # Do next_guess
+            idx, value = next_guess
+            s.cells[idx].candidates = value
+            s.propagate()
+
+            # If still solvable, push to stack, else iterate
+            if s.solved():
+                self.solutions.append(str(s))
+                stack.pop()
+            if not s.valid():
+                stack.pop()
+            else:
+                stack.append((s.serialize(), None))
+        """
 
     def propagate(self):
-        """Propagate"""
-        h = ()
+        """
+        Propagate constraints:
+            (1) If a cell holds a value, no other cell in unit can hold that value.
+            (2) If a value can only be held by a single cell in unit, that cell can not hold another value.
+        """
+        h = 81 * 9
         while self.get_hash() != h:
             h = self.get_hash()
             print('Propagating with hash =', h)
+            # Propagate (1)
             for cell in self.cells:
                 cell.propagate_to_peers()
-        # TODO check if still solvable
+            # Propagate (2)
+            # for unit in self.units:
+            #    unit.propagate()
 
-    def get_hash(self) -> Tuple[int, int]:
+    def get_hash(self) -> int:
         """Calculate hash to detect changes"""
-        filled = sum([1 for cell in self.cells if cell.value is not None])
         candidates = sum([len(cell.candidates) for cell in self.cells])
-        return filled, candidates
+        return candidates
 
-    def solvable(self) -> bool:
-        """Checks if Sudoku still solveable"""
+    def valid(self) -> bool:
+        """Checks if Sudoku still solvable"""
         return all([cell.valid() for cell in self.cells])
 
-    def __str__(self) -> str:
+    def solved(self):
+        """Checks if Sudoku is solved"""
+        return all([cell.solved() for cell in self.cells])
+
+    def serialize(self) -> List[str]:
+        """Return a serialized form"""
+        return [cell.candidates for cell in self.cells]
+
+    @staticmethod
+    def deserialize(candidates: List[str]) -> Sudoku:
+        """Return a serialized form"""
+        s = Sudoku()
+        s.cells = []
+        for i in range(len(candidates)):
+            cell = Cell(candidates, i)
+            s.cells.append(cell)
+        return s
+
+    def printable_str(self) -> str:
         """Print the Sudoku"""
         c = 0
         r = 0
@@ -89,6 +157,10 @@ class Sudoku:
             elif c in [3, 6]:
                 s += '| '
         return s
+
+    def __str__(self) -> str:
+        """Print the Sudoku"""
+        return ''.join(str(c) for c in self.cells).replace(' ', '.')
 
     def show(self):
         """Show GUI"""
