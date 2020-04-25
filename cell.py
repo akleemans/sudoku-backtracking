e65c78ce8c638959bcce1f16639bc622351ea910
Adrianus Kleemans <a.kleemans@gmail.com>
2020-04-25 09:51:23 +0200

    Refactor cell to only hold candidates, not value

--- cell.py
+++ cell.py
@@ -5,56 +5,48 @@ from typing import List, Set, Optional
 
 class Cell:
     peers: Set[Cell] = set()
-    value: Optional[str] = None
-    candidates: str = ''
+    candidates: str = '123456789'
     initial: bool = False
     color: str = 'blue'
-    id: int
+    cell_id: int
 
-    def __init__(self, value, id: int):
-        self.id = id
+    def __init__(self, value: str, cell_id: int):
+        self.cell_id = cell_id
         if value != '.':
-            self.value = value
+            self.candidates = value
             self.initial = True
             self.color = 'black'
-        else:
-            self.candidates = '123456789'
 
-    def add_peers(self, peers: List[Cell]) -> None:
+    def add_peers(self, peers: Set[Cell]) -> None:
         """Convenience method to add peers"""
-        self.peers = self.peers | set(peers)
-        print(self.id, 'has now peers:', len(self.peers))
+        self.peers = self.peers | peers
+        print(self.cell_id, 'has now # of peers:', len(self.peers))
 
     def propagate_to_peers(self) -> None:
         """Propagate own number to peers (if set)"""
-        if self.value is not None:
+        if len(self.candidates) == 1:
             for peer in self.peers:
-                peer.remove_candidate(self.value)
+                peer.remove_candidate(self.candidates)
 
     def remove_candidate(self, value: str) -> None:
-        """Remove candidate"""
+        """Remove value from candidates"""
         if value in self.candidates:
-            print(self.id, 'Removing', value, 'from candidates:', self.candidates)
+            print(self.cell_id, 'Removing', value, 'from candidates:', self.candidates)
             self.candidates = self.candidates.replace(value, '')
-            if len(self.candidates) == 1:
-                # Has to be the solution
-                self.value = self.candidates
-                self.candidates = ''
 
     def __str__(self) -> str:
         """Return printable value"""
-        return self.value if self.value is not None else ' '
+        return self.candidates if len(self.candidates) == 1 else ' '
 
-    def reset(self) -> None:
-        """Reset cell to start state"""
-        if not self.initial:
-            self.value = None
-            self.candidates = '123456789'
+#    def reset(self) -> None:
+#        """Reset cell to start state"""
+#        if not self.initial:
+#            self.candidates = '123456789'
 
     def valid(self) -> bool:
         """Returns if cell still valid"""
-        if self.value is not None:
-            return True
-        else:
-            # Check if any candidates are left
-            return len(self.candidates) >= 1
+        return len(self.candidates) >= 1
+
+    def solved(self) -> bool:
+        """Returns if cell has a value"""
+        return len(self.candidates) == 1
