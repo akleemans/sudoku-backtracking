e9064f2acf4c9af9f7169197a3e7bab3ea54ddfa
Adrianus Kleemans <a.kleemans@gmail.com>
2020-04-26 20:17:42 +0200

    Add Sudoku test sets

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from solver import Solver
 
 
 def solve_all(filename: str, puzzles_only=False):
+    """Read a csv file and solve all Sudokus in it.
+    'puzzles_only' specifies if solutions should be checked."""
+    print('\nSolving', filename, '.......')
     with open(filename) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',', quotechar='"')
         sudoku_rows = [row for row in csv_reader]
@@ -13,28 +16,26 @@ def solve_all(filename: str, puzzles_only=False):
     total_solving_time = 0
     count = 0
     for sudoku_row in sudoku_rows:
-        if puzzles_only:
-            puzzle = sudoku_row[0]
-        else:
-            puzzle, expected_solution, difficulty, nr = sudoku_row
+        puzzle = sudoku_row[0]
 
         start = time.time()
         solutions = Solver.solve(puzzle)
         end = time.time()
         if len(solutions) != 1:
             print('!!! Got multiple solutions:', solutions)
-        if not puzzles_only and solutions.pop() != expected_solution:
+        found_solution = solutions.pop()
+        if not puzzles_only and found_solution != sudoku_row[1]:
             print('!!! Got a different solution than real solution :((')
         solving_time = end - start
         total_solving_time += solving_time
-        print('Solved', count, ('/diff:' + str(difficulty) if not puzzles_only else ''), 'in', round(solving_time, 8),
-              's')
-        # print('Solved', count, 'with difficulty', difficulty, 'in', round(solving_time, 8), 's')
+        print('Solved', count, 'in', round(solving_time, 8), 's')
         count += 1
     print('Solved all in', round(total_solving_time, 8), 's')
 
 
 def solve_single(sudoku_str: str):
+    """Solve a single Sudoku and show solution"""
+    print('\nSolving', sudoku_str)
     start = time.time()
     solutions = Solver.solve(sudoku_str)
     if len(solutions) == 0:
@@ -49,9 +50,12 @@ def solve_single(sudoku_str: str):
 
 
 if __name__ == "__main__":
-    # solve_all('hard-magictour95.csv', True)
-    # solve_all('sudokus2.csv')
+    # First of "simple" Sudokus (magictour)
     # solve_single('.94...13..............76..2.8..1.....32.........2...6.....5.4.......8..7..63.4..8')
 
-    # first of Norvigs "hard" puzzles
-    solve_single('4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......')
+    # First of Norvigs "hard" puzzles
+    # solve_single('4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......')
+
+    solve_all('magictour_easy.csv', True)
+    solve_all('magictour_hard.csv', True)
+    solve_all('menneske_random.csv')
